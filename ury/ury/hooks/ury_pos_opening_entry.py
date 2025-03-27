ae5e98f0e417e24890f1a1780d77b65197f5d3f9
Sanjusha_tridz <sanjusha@tridz.com>
2025-03-27 19:37:30 +0000

    Fix:Fetching current time in pos opening entry

--- ury/ury/hooks/ury_pos_opening_entry.py
+++ ury/ury/hooks/ury_pos_opening_entry.py
@@ -1,10 +1,13 @@
 import frappe
+from frappe.utils import today
+from frappe.utils import  get_datetime,today,now
 
 def validate(doc,method):
     set_cashier_room(doc,method)
     
 def before_save(doc, method):
     main_pos_open_check(doc, method)
+    set_current_time(doc,method)
     
     
 def set_cashier_room(doc,method):
@@ -24,8 +27,15 @@ def set_cashier_room(doc,method):
                     'room': room['room']
                 })
 
+def set_current_time(doc,method):
+    multiple_cashier = frappe.db.get_value("POS Profile",doc.pos_profile,"custom_enable_multiple_cashier")
+    if multiple_cashier:
+        date_time = now()
+        doc.period_start_date = date_time
+
 def main_pos_open_check(doc,method):
     owner = None
+    current_date = today()
     multiple_cashier = frappe.db.get_value("POS Profile",doc.pos_profile,"custom_enable_multiple_cashier")
     if multiple_cashier:
         get_cashier = frappe.get_doc("POS Profile", doc.pos_profile)
@@ -37,7 +47,7 @@ def main_pos_open_check(doc,method):
             pos_opening_list = frappe.get_all(
                 "POS Opening Entry",
                 fields=["name", "docstatus", "status", "posting_date"],
-                filters={"branch": doc.branch,"user":owner},
+                filters={"branch": doc.branch,"user":owner,"posting_date":current_date},
             )
             flag = 1
             for pos_opening in pos_opening_list:
